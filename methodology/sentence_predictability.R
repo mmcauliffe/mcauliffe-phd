1f48af0d9a523af6f257e863fa2ebfa2ba683c00
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2014-06-25 17:13:51 -0700

    Some sentence stuff

--- methodology/sentence_predictability.R
+++ methodology/sentence_predictability.R
@@ -3,7 +3,7 @@
 library(ggplot2)
 library(plyr)
 
-senpred <- read.delim('C:\\Users\\michael\\Dropbox\\Michael_Dissertation\\Pretest\\senpred_parsed.txt')
+senpred <- read.delim('senpred_parsed.txt')
 senpred$RespProp <- as.numeric(as.character(senpred$RespProp))
 summary(senpred)
 
@@ -14,3 +14,6 @@ ddply(senpred,~Predictive,summarise,mean(RespProp))
 par(mfrow=c(1,2))
 ggplot(senpred,aes(x=RespProp,colour=Predictive))+geom_density()
 ggplot(senpred,aes(x=PresentProp,colour=Predictive))+geom_density()
+
+sensum <- ddply(senpred,~Predictive*Word,summarise,MeanRespProp = mean(RespProp),MeanPresentProp=mean(PresentProp))
+questionable <- subset(sensum,Predictive=='yes'& MeanPresentProp < 0.5)
--- methodology/sentences/sentences.py
+++ methodology/sentences/sentences.py
@@ -2,24 +2,27 @@ import os
 import csv
 from collections import Counter
 
-PRETEST_DIR = r'C:\Users\michael\Dropbox\Michael_Dissertation\Pretest'
+PRETEST_DIR = r''
 
 senpred = []
 
-with open(os.path.join(PRETEST_DIR,'senpred_results.txt'),'r') as f:
+with open(os.path.join('senpred_results.txt'),'r') as f:
     reader = csv.DictReader(f,delimiter='\t')
     for line in reader:
         senpred.append(line)
 
 bag_of_words = {}
+responses = {}
 inout = {}
 
 for l in senpred:
     if (l['Word'],l['Predictive']) not in bag_of_words:
         bag_of_words[(l['Word'],l['Predictive'])] = []
         inout[(l['Word'],l['Predictive'])] = []
+        responses[(l['Word'],l['Predictive'])] = []
     
     initial_bag = [x for x in l['RESP'].split(',') if x != '']
+    responses[(l['Word'],l['Predictive'])] += [x for x in l['RESP'].split(',')]
     final_bag = []
     for w in initial_bag:
         if ' ' in w:
@@ -35,7 +38,7 @@ for l in senpred:
     
 output = {}
 for k,v in bag_of_words.items():
-    
+    responses[k] = Counter(responses[k])
     nums = Counter(v)
     desired = 0
     if k[0] in nums:
@@ -43,10 +46,15 @@ for k,v in bag_of_words.items():
     present = sum(inout[k])/len(inout[k])
     output[k] = (desired / sum([x for x in nums.values()]),present)
     
-with open(os.path.join(PRETEST_DIR,'senpred_parsed.txt'),'w') as f:
+with open(os.path.join('senpred_parsed.txt'),'w') as f:
     writer = csv.writer(f,delimiter='\t')
     writer.writerow(['Word','Predictive','RespProp','PresentProp'])
     for k,v in output.items():
         writer.writerow([k[0],k[1],v[0],v[1]])
-print(output)
+        
+#print(output)
 #print(sentence_dict)
+for k,v in responses.items():
+	if output[k][1] < 0.5 and k[1] == 'yes':
+		print(k,v)
+		print('\n')
