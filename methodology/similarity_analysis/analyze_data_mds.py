ee524fff6f62c0b6c4e3dc0c4f008340b5e9c9b7
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2015-04-30 22:59:23 -0700

    MDS

---
+++ methodology/similarity_analysis/analyze_data_mds.py
@@ -0,0 +1,91 @@
+import csv
+import os
+from functools import partial
+
+from acousticsim.main import analyze_directory
+
+from acousticsim.helper import get_vowel_points
+
+from acousticsim.distance.dtw import dtw_distance
+from acousticsim.clustering import ClusterNetwork
+
+data_dir = r'C:\Users\michael\Documents\Data\Dissertation\Cluster'
+
+def word_distance(rep_one, rep_two):
+    base, _ = os.path.splitext(rep_one._filepath)
+    one_textgrid = base + '.TextGrid'
+    one_begin,one_end = get_vowel_points(one_textgrid,
+                tier_name = 'word', vowel_label = 'word')
+
+    base, _ = os.path.splitext(rep_two._filepath)
+    two_textgrid = base + '.TextGrid'
+    two_begin,two_end = get_vowel_points(two_textgrid,
+                    tier_name = 'word', vowel_label = 'word')
+
+    return dtw_distance(rep_one[one_begin, one_end], rep_two[two_begin, two_end])
+
+def sibilant_distance(rep_one, rep_two):
+    base, _ = os.path.splitext(rep_one._filepath)
+    one_textgrid = base + '.TextGrid'
+    one_begin,one_end = get_vowel_points(one_textgrid,
+                tier_name = 'sibilant', vowel_label = 'sibilant')
+
+    base, _ = os.path.splitext(rep_two._filepath)
+    two_textgrid = base + '.TextGrid'
+    two_begin,two_end = get_vowel_points(two_textgrid,
+                    tier_name = 'sibilant', vowel_label = 'sibilant')
+
+    return dtw_distance(rep_one[one_begin, one_end], rep_two[two_begin, two_end])
+
+def save_output(cn):
+    with open('output.txt', 'w') as f:
+        writer = csv.writer(f, delimiter = '\t')
+        writer.writerow(['X', 'Y', 'Experiment', 'Type', 'Word'])
+
+        for n in cn:
+            x, y = n['pos']
+            label = n['rep']._true_label
+            path = n['rep']._filepath
+            name = os.path.basename(path)
+            name, ext = os.path.splitext(name)
+            if 'Categ' not in label:
+                name = name.split('_')[-1]
+            exp , t = label.split('_')
+            writer.writerow([x, y, exp.lower(), t, name])
+
+
+def main():
+    kwargs = {'rep':'mfcc',
+                'match_function':sibilant_distance,
+                'use_multi':True,
+                'num_cores':4,
+                'return_rep': True,
+                }
+    scores, reps = analyze_directory(data_dir, **kwargs)
+
+    cn = ClusterNetwork(reps)
+
+
+    cluster_method = 'affinity'
+    one_cluster = False
+
+    cn.cluster(scores,cluster_method,one_cluster)
+    save_output(cn)
+
+def debug():
+    directories = list()
+    all_files = list()
+    wavs = list()
+    for f in os.listdir(data_dir):
+        print(f)
+        path = os.path.join(data_dir,f)
+        all_files.append(path)
+        if f.lower().endswith('.wav'):
+            wavs.append(path)
+        if os.path.isdir(path):
+            directories.append(path)
+    print(directories)
+
+if __name__ == '__main__':
+    main()
+    #debug()
--- results/expose.R
+++ results/expose.R
@@ -6,7 +6,7 @@ summary(aov(WordResp ~ Attention*itemtype,subset(subj.tolerances,Experiment == '
 summary(aov(WordResp ~ Attention*itemtype,subset(subj.tolerances,Experiment == 'exp2')))
 ### EXPERIMENT 1
 
-experiment.1.expose.mod <- glmer(ACC ~ cTrial*itemtype2*Attention*ExposureType + (1+cTrial*itemtype2|Subject) + (1+Attention*ExposureType|Word), family='binomial',data=subset(expose.word,Experiment=='exp2'), control=glmerControl(optCtrl=list(maxfun=2000000) ))
+experiment.1.expose.mod <- glmer(ACC ~ cTrial*itemtype2*Attention*ExposureType + (1+cTrial*itemtype2|Subject) + (1+Attention|Word), family='binomial',data=subset(expose.word,Experiment=='exp2'), control=glmerControl(optCtrl=list(maxfun=2000000) ))
 summary(experiment.1.expose.mod)
 
 experiment.1.expose.mod.trimmed <- glmer(ACC ~ cTrial*itemtype2*Attention*ExposureType +(1|Subject) + (0+cTrial*itemtype2|Subject) + (1+Attention|Word), family='binomial',data=subset(expose.word,Experiment=='exp2'), control=glmerControl(optCtrl=list(maxfun=2000000) ), subset = abs(scale(resid(experiment.1.expose.mod))) < 2.5)
@@ -23,7 +23,7 @@ summary(experiment.1.expose.mod.rt.trimmed)
 
 ### EXPERIMENT 2
 
-experiment.2.expose.mod <- glmer(ACC ~ cTrial*itemtype2*Attention*ExposureType + (1+cTrial*itemtype2|Subject) + (1+Attention*ExposureType|Word), family='binomial',data=subset(expose.word,Experiment=='exp1'), control=glmerControl(optCtrl=list(maxfun=200000) ))
+experiment.2.expose.mod <- glmer(ACC ~ cTrial*itemtype2*Attention*ExposureType +(1|Subject) + (0+cTrial+itemtype2|Subject) + (1+Attention*ExposureType|Word), family='binomial',data=subset(expose.word,Experiment=='exp1'), control=glmerControl(optCtrl=list(maxfun=200000) ))
 summary(experiment.2.expose.mod)
 
 experiment.2.expose.mod.trimmed <- glmer(ACC ~ cTrial*itemtype2*Attention*ExposureType + (1+cTrial*itemtype2|Subject) + (1+Attention|Word), family='binomial',data=subset(expose.word,Experiment=='exp1'), control=glmerControl(optCtrl=list(maxfun=200000) ), subset = abs(scale(resid(experiment.2.expose.mod))) < 2.5)
